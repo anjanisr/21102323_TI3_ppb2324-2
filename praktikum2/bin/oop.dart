623d55fc55ce531cf8ee1151c67ca9c119a79992
anjanisr <anjanislsbla@gmail.com>
2024-03-08 19:55:30 +0700

    commit

---
+++ praktikum2/bin/oop.dart
@@ -0,0 +1,1 @@
+// void main(list<string> argumenst){}
---
+++ praktikum2/bin/praktikum2.dart
@@ -0,0 +1,110 @@
+// //Hello World
+void main() {
+//   print('Hello, World');
+// }
+
+//Membuat variable
+//   var name = 'Anjani';
+//   var year = 2001;
+//   var bulan = ['Januari', 'Februari', 'Maret', 'April'];
+//   print('$name $year $bulan');
+// }
+
+// //Membuat variable
+//   String greetings = 'Hello Anjani!';
+//   int tahun = 2024;
+//   print('$greetings $tahun');
+// }
+
+// //if and else
+//   var angka = 10;
+//   if (angka > 0) {
+//     print('bilangan positif');
+//   } else if (angka < 0) {
+//     print('bilangan negatif');
+//   }
+// }
+//   //switch case
+//   String fruit = 'peach';
+//   switch (fruit) {
+//     case 'apple':
+//       print('Selected fruit is an apple');
+//       break;
+//     case 'banana':
+//       print('Selected fruit is a banana');
+//       break;
+//     default:
+//       print('Selected fruit is not recognized');
+//   }
+// }
+
+// //for loops
+//   for (int i = 1; i <= 5; i++) {
+//     print('$i');
+//   }
+// }
+
+//   //while and do while
+//   var i = 5;
+//   while (i <= 8) {
+//     print(i);
+//     i++;
+//   }
+// }
+
+//   var k = 8;
+//   do {
+//     print(k);
+//     k++;
+//   } while (k <= 10);
+// }
+//   //list
+//   List<int> numbers = [6, 7, 8, 9, 10];
+//   print('list angka:');
+//   for (var number in numbers) {
+//     print(number);
+//   }
+// }
+
+//   //dynamic list
+//   List dynamicList = [1, 'Informatika', true];
+//   print('list:');
+//   for (var item in dynamicList) {
+//     print(item);
+//   }
+// }
+
+//   //Tanpa spread operator
+//   var buah = ['Mangga'];
+//   var hewan = ['Kucing'];
+//   var allFavorites = [buah, hewan];
+//   print(allFavorites);
+// }
+
+//   //Dengan spread operator
+//   var fruits = ['Manggis'];
+//   var animal = ['Ayam'];
+//   var kesukaan = [...fruits, ...animal];
+//   print(kesukaan);
+// }
+
+//   //set
+//   var angkaSet = {1, 4, 6};
+//   Set<int> bilanganSet = new Set.from([1, 4, 6, 4, 1]);
+//   print(bilanganSet);
+// }
+
+  //Map
+  var kota = {
+    'Semarang': 'Jawa Tengah',
+    'Bandung': 'Jawa Barat',
+    'Malang': 'Jawa Timur'
+  };
+  print('kota:');
+  kota.forEach((name, province) {
+    print('$name provinsi $province');
+  });
+}
+
+  //OOP class
+
